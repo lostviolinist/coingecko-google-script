8d949e86faf9d2de0e54d58ba40c47f68250676a
lostviolinist <50481227+lostviolinist@users.noreply.github.com>
2020-11-05 16:11:24 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('My Custom Menu')
+      .addItem('CoinGecko', 'getCoinGeckoData')
+      .addToUi();
+}
+
+  Browser.msgBox("Opening function");
+  
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName("CoinGecko");
+  var url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+  var json = getCoinGeckoData(url);
+  
+  if (json[0] === "Error:") {
+    // deal with error with fetch operation
+    Browser.msgBox("Error");
+  }
+  else {
+    if (json[0] !== 200) {
+      // deal with error from api
+      var data = json[0]
+      
+      // parse into array for Google Sheet
+      var outputData = [
+        ["id: ", data.id],
+        ["current price: ", data.current_price],
+        ["market cap: ", data.market_cap],
+      ];
+        Browser.msgBox(outputData);
+        
+        sheet.getRange(1,1).setValues(outputData);
+        }
+   }
+
+
+function getCoinGeckoData(url) {
+        
+  try {
+    var response = UrlFetchApp.fetch(url);
+    var responseData = response.getContentText();
+    var json = JSON.parse(responseData);
+    return json;
+  }
+  catch (e) {
+    Logger.log(e);
+    return ["Error:", e];
+  }
+}
